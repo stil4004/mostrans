0f863a2143452b5b62a979b22fa8cf12155a035b
akira <stil4004@gmail.com>
2024-04-20 22:45:23 +0300

    check for logs

--- cmd/mostrans/main.go
+++ cmd/mostrans/main.go
@@ -20,14 +20,14 @@ func main() {
 	app.Post("/api/admin/login", func(c *fiber.Ctx) error {
 		log.Println(string(c.Body()))
 		var resp Response = Response{
-			Data: "Иди нахуй",
+			Data: "все неправильно, чмо",
 		}
 		return c.Status(200).JSON(resp)
 	})
 	app.Post("/chat", func(c *fiber.Ctx) error {
 		log.Println(string(c.Body()))
 		var resp Response = Response{
-			Data: "че нахуй",
+			Data: "хуйню сказала",
 		}
 		return c.Status(200).JSON(resp)
 	})
